0ee0a7060686ec7280a9257ac85dfcadd9f58571
Geoff Turk <geoff@ic3.dev>
2025-03-31 22:11:55 +0200

    fix: remove unneeded GH action

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit CFW</h1>
+<h1>Welcome to SvelteKit Cloudflare Worker</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
